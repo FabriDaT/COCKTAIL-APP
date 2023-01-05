7b8e9b820885e6b08a6629b5742fb1e23ad2dfb8
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2023-01-05 12:48:15 -0300

    commit

--- filter.js
+++ filter.js
@@ -1,7 +1,35 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+document.querySelector('a').addEventListener("click", (e)=>{
+  console.log((e).getAttribute('value'))
+}
+)
+console.log(document.querySelector('a').value)
+ 
+
+
+
 
 
     //FILTER BY ALCOHOL
-    filterCocktails('rum');
+    filterCocktails(document.querySelector('#dropdown').value);
     function filterCocktails(alcohol){
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`)
         .then((response) => {
